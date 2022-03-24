ae852285bc418a77d26a2ffe18e88eede078416c
Subash Devarajan <subash04012001@gmail.com>
2022-03-25 01:25:14 +0530

    AppBar Template included : yet to be customized

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import ResponsiveAppBar from './sideBar';
+// import ResponsiveAppBar from './sideBar';
 import SubjectInfo from './components/getSubjectInfo';
 import Footer from './components/footer';
 import QPInputPanel from './components/qpInputPanel';
+import ResponsiveAppBar from './components/navbar';
 // import 
 // import { ProSidebar } from 'react-pro-sidebar';
 
 function App() {
   return (
     <div className="App">
-      {/* <ResponsiveAppBar></ResponsiveAppBar> */}
+      <ResponsiveAppBar></ResponsiveAppBar>
       <div>
+        {/* <HamburgerMenuPage /> */}
       <QPInputPanel />
       </div>
       <div class="fixed-bottom">
